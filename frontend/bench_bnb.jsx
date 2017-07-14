2f845608a708d8948b057cb24a4caadaa06e84f9
boothandrewd <boothandrewd@gmail.com>
2017-07-14 16:40:40 -0700

    Complete auth back and middlestack

--- frontend/bench_bnb.jsx
+++ frontend/bench_bnb.jsx
@@ -1,7 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+// TODO: Remove testing imports
+import * as APIUtil from './util/session_api_util';
+
 document.addEventListener('DOMContentLoaded', () => {
   const root = document.getElementById('root');
   ReactDOM.render(<h1>Welcome to BenchBnB</h1>, root);
+
+  // TODO: Remove testing window objects
+  window.user = {
+    username: 'adbooth',
+    password: '123456'
+  };
+  window.signUp = APIUtil.signUp;
+  window.signIn = APIUtil.signIn;
+  window.signOut = APIUtil.signOut;
 });
